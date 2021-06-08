7af83ee84170bc61c6a885c830f7a132afb639e6
juneadkhan <junead@live.unc.edu>
2021-06-08 18:36:17 +0100

    Mobile first improvement

--- workExperience.js
+++ workExperience.js
@@ -12,7 +12,7 @@ export const loadDOM = function (listOfJobs) {
     <div class="workCard mb-8 md:flex">
     <div class="md:w-1/3 mr-12 flex-none">
     <figure class="image mb-3 mr-5">
-    <img src=${job.img} alt="Placeholder image">
+    <a href=${job.link} target="_blank"><img src=${job.img} alt="Placeholder image"></a>
   </figure>
       <p class="m-0 mt-1 leading-normal text-xl text-white opacity-50 font-bold">${job.jobTitle}</p>
       <p class="m-0 mt-1 leading-normal text-base text-white opacity-50">${job.dates}</p>
@@ -46,9 +46,10 @@ export const loadDOM = function (listOfJobs) {
     dates: "June 2021 to Present",
     location: "Bournemouth, UK",
     img: "https://jpmcsso.jpmorgan.com/sso/custom/web/components/APEX/images/Logo2008_JPMC_White.png",
-    bullet1: "",
+    link: "https://www.jpmorganchase.com/",
+    bullet1: "CIB Architecture Governance",
     bullet2: "",
-    tag: ''
+    tag: 'Java'
 
   
     
@@ -60,9 +61,10 @@ export const loadDOM = function (listOfJobs) {
     dates: "February 2021 to Present",
     location: "London, UK",
     img: "https://www.lshtm.ac.uk/themes/custom/lshtm/images/logo-white.svg",
+    link: "https://www.lshtm.ac.uk/",
     bullet1: "Collaborated with international partners across 3 countries (UK, US and China) for a study on elderly sexual health. Co-authored studies and directed analysis of the NATSAL and ELSA databases to garner and communicate insights from the largest and most detailed scientific studies of sexual behaviour in the world",
     bullet2: "",
-    tag: "SAS/Python/Latent_Class_Analysis/Excel/JMP",
+    tag: "SAS, Python, Latent Class Analysis, Excel, JMP",
     tag1: `<span class="tag is-link is-light" style="background: #38373d;  color: #E2D5FE;">SAS</span>`,
     tag2: `<span class="tag is-link is-light" style="background: #38373d;  color: #E2D5FE;">Python</span>`,
     tag3: `<span class="tag is-success is-light" style="background: #38373d;  color: #E2D5FE;">Latent Class Analysis</span>`,
@@ -81,9 +83,10 @@ export const loadDOM = function (listOfJobs) {
     dates: "July 2020 to August 2020",
     location: "Remote, USA",
     img: "https://www.aptusdatalabs.com/wp-content/uploads/2020/10/1200px-Google_Cloud_Logo.svg.png",
+    link: "https://cloud.google.com/",
     bullet1: "Guided by Google Engineers to use Google Cloud Platform tools such as BigQuery, DataPrep and Data Studio. Analysed over 20 large public datasets using advanced SQL and other GCP tools to garner and communicate insights.",
     bullet2: "",
-    tag: "SQL/Python/Machine_Learning/Cloud_Computing/BigQuery/Google_Cloud_Platform",
+    tag: "SQL, Python, Machine Learning, BigQuery, Google Cloud Platform",
     tag1: `<span class="tag is-link is-light" style="background: #38373d;  color: #E2D5FE;">SQL</span>`,
     tag2: `<span class="tag is-link is-light" style="background: #38373d;  color: #E2D5FE;">Python</span>`,
     tag3: `<span class="tag is-success is-light" style="background: #38373d;  color: #E2D5FE;">Machine Learning</span>`,
@@ -101,9 +104,10 @@ export const loadDOM = function (listOfJobs) {
     dates: "February 2020 to July 2020",
     location: "Chapel Hill, NC",
     img: "https://d1gntqhqj0rbcs.cloudfront.net/assets/111/whiteunclogo.png",
+    link: "https://www.unc.edu/",
     bullet1: "Reduced downtime 15% by working with a team of 12 technicians to maintain ITS Hardware/Software in UNC Classrooms",
     bullet2: "",
-    tag: "Hardware/Tech_Support",
+    tag: "Hardware, Tech Support",
     tag1: `<span class="tag is-success is-light" style="background: #38373d;  color: #E2D5FE;">Hardware</span>`,
     tag2: `<span class="tag is-success is-light" style="background: #38373d;  color: #E2D5FE;">Tech Support</span>`,
     tag3: ``,
